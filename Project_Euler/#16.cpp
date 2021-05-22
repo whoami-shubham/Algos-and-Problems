535692237798f71f67a46b66045d93d16d5ade5f
Shubham Jha <Shubhamkumarjha0013@gmail.com>
2021-05-22 13:00:54 +0530

    :tada: xPowN

--- Project_Euler/#16.cpp
+++ Project_Euler/#16.cpp
@@ -25,3 +25,52 @@ unsigned ll digit_sum(unsigned ll n){
     }
     return sum;
 }
+/*
+another solution
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> xPowN(int x, int n){
+    vector<int>ans;
+    ans.push_back(1);
+    for(int i=1;i<=n;i++){
+            int temp = ans[0]*x;
+            ans[0] = (temp)%10;
+            int carry = temp/10;
+            for(int j=1;j<ans.size();j++){
+               temp = (ans[j]*x + carry);
+               ans[j] = temp%10;
+               carry = temp/10;
+            }
+            if(carry){
+                ans.push_back(carry);
+            }
+    }
+    return ans;
+}
+int digitSum(int n){
+    vector<int>digits = xPowN(2,n);
+    int ans = 0;
+    for(auto d:digits){
+        ans += d;
+    }
+    return ans;
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int testCases;
+    cin>>testCases;
+    while(testCases>0){
+        int n;
+        cin>>n;
+        cout<<digitSum(n)<<endl;
+        testCases--;
+    }
+    return 0;
+}
